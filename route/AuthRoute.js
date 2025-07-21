86f9d3317e2d502db6913db5c1c322f16125a4d8
ashrafmakandar <37826738+ashrafmakandar@users.noreply.github.com>
2025-07-21 14:36:51 +0530

    fiexed swagger docs

--- route/AuthRoute.js
+++ route/AuthRoute.js
@@ -1,3 +1,47 @@
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User registered
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   get:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: password
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Authenticated
+ */
 
 const {loginUser,registerUser } = require("../contoller/AuthController");
 
